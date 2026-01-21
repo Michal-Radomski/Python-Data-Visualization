0734db9ce03cf1f5f1669d5d62260299a9c0ae5a
Michal Radomski <michalra@op.pl>
2026-01-21 21:21:08 +0100

    Data Visualization with Python and Bokeh course - commit 16

--- 02 Bokeh/01/iris2.py
+++ 02 Bokeh/01/iris2.py
@@ -1,12 +1,26 @@
 # Importing libraries
 from bokeh.io import output_file, show  # type: ignore[import-not-found]
-from bokeh.models import ColumnDataSource  # type: ignore[import-not-found]
+from bokeh.models import (  # type: ignore[import-not-found]
+    ColumnDataSource,
+    HoverTool,
+    LabelSet,
+    PanTool,
+    ResetTool,
+)
 from bokeh.plotting import figure  # type: ignore[import-not-found]
 from bokeh.sampledata.iris import flowers  # type: ignore[import-not-found]
 
 colormap = {"setosa": "red", "versicolor": "green", "virginica": "blue"}
 flowers["color"] = [colormap[x] for x in flowers["species"]]
 flowers["size"] = flowers["sepal_width"] * 4
+
+
+urlmap = {
+    "setosa": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Kosaciec_szczecinkowaty_Iris_setosa.jpg/800px-Kosaciec_szczecinkowaty_Iris_setosa.jpg",
+    "versicolor": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Blue_Flag%2C_Ottawa.jpg/800px-Blue_Flag%2C_Ottawa.jpg",
+    "virginica": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Iris_virginica.jpg/800px-Iris_virginica.jpg",
+}
+flowers["imgs"] = [urlmap[x] for x in flowers["species"]]
 
 setosa = ColumnDataSource(flowers[flowers["species"] == "setosa"])
 versicolor = ColumnDataSource(flowers[flowers["species"] == "versicolor"])
@@ -48,6 +62,54 @@ f.circle(
     legend_label="Virginica",
     source=virginica,
 )
+
+labels = LabelSet(
+    x="petal_length",
+    y="petal_width",
+    text="sepal_length",
+    level="glyph",
+    x_offset=5,
+    y_offset=5,
+    source=virginica,
+)
+
+f.add_layout(labels)
+
+# Style the tools
+f.tools = [PanTool(), ResetTool()]
+hover = HoverTool(
+    tooltips="""
+     <div>
+            <div>
+                <img
+                    src="@imgs" height="42" alt="@imgs" width="42"
+                    style="float: left; margin: 0px 15px 15px 0px;"
+                    border="2"
+                />
+            </div>
+            <div>
+                <span style="font-size: 15px; font-weight: bold;">@species</span>
+            </div>
+            <div>
+                <span style="font-size: 10px; color: #696;">Petal length: @petal_length</span><br>
+                <span style="font-size: 10px; color: #696;">Petal width: @petal_width</span>
+            </div>
+        </div>
+"""
+)
+f.add_tools(hover)
+f.toolbar_location = "above"
+f.toolbar.logo = None
+
+# Style the legend
+f.legend.location = (575, 555)
+f.legend.location = "top_left"
+f.legend.background_fill_alpha = 0
+f.legend.border_line_color = None
+f.legend.margin = 10
+f.legend.padding = 18
+f.legend.label_text_color = "olive"
+f.legend.label_text_font = "times"
 
 # Save and show the figure
 show(f)
